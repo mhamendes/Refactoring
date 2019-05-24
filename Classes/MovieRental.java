aa96b0f78f73d7ae7b17a700ff3cfe6dc228c42f
mhamendes <48832695+mhamendes@users.noreply.github.com>
2019-05-24 00:20:45 -0300

    Fourth Commit - Rename

--- Classes/MovieRental.java
+++ Classes/MovieRental.java
@@ -56,6 +56,25 @@ class Customer {
       return _name;
    };
 
+   private double amountFor(Rental aRental) {
+      double result = 0;
+      switch (aRental.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            result += 2;
+            if (aRental.getDaysRented() > 2)
+               result += (aRental.getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            result += aRental.getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            result += 1.5;
+            if (aRental.getDaysRented() > 3)
+               result += (aRental.getDaysRented() - 3) * 1.5;
+            break;
+      }
+      return result;
+   }
 
    public String statement() {
      double totalAmount = 0;
@@ -85,24 +104,4 @@ class Customer {
                 " frequent renter points";
       return result;
   }
-}
-
-private double amountFor(Rental aRental) {
-   double result = 0;
-   switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         result += 2;
-         if (aRental.getDaysRented() > 2)
-            result += (aRental.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         result += aRental.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         result += 1.5;
-         if (aRental.getDaysRented() > 3)
-            result += (aRental.getDaysRented() - 3) * 1.5;
-         break;
-   }
-   return result;
 }
