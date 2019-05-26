c4cf2f58f4771817d70d811b5dc2ee6886926c05
mhamendes <48832695+mhamendes@users.noreply.github.com>
2019-05-26 15:55:12 -0300

    Third Commit - Extract Method

--- Classes/Costumer.java
+++ Classes/Costumer.java
@@ -13,6 +13,25 @@ class Customer {
       return _name;
    };
 
+   private double  amountFor(){
+     //determine amounts for each line
+     switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+              thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+          case Movie.NEW_RELEASE:
+              thisAmount += each.getDaysRented() * 3;
+          break;
+          case Movie.CHILDRENS:
+              thisAmount += 1.5;
+              if (each.getDaysRented() > 3)
+                  thisAmount += (each.getDaysRented() - 3) * 1.5;
+          break;
+      }
+    }
+
   public String statement() {
      double totalAmount = 0;
      int frequentRenterPoints = 0;
@@ -22,22 +41,7 @@ class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
